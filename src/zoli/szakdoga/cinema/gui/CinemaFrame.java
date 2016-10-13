bda65e66a8d4f04b86e5452ffdc3fc48d7c52173
cantiii <cantiii19@gmail.com>
2016-10-13 16:28:57 +0200

    menü panel

--- src/zoli/szakdoga/cinema/gui/CinemaFrame.java
+++ src/zoli/szakdoga/cinema/gui/CinemaFrame.java
@@ -1,11 +1,62 @@
 package zoli.szakdoga.cinema.gui;
 
-import java.awt.Frame;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.*;
+import static zoli.szakdoga.cinema.gui.GuiConstants.*;
 
 /**
  *
  * @author Zoli
  */
-public class CinemaFrame extends Frame {
+public class CinemaFrame extends JFrame {
     
+    private final JPanel jPanel = new JPanel();
+    
+    public CinemaFrame() {
+        initFrame();
+
+        setMenu();
+        setCenter();
+
+        pack();
+        setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        setLocationRelativeTo(null);
+    }
+    
+    private void initFrame() {
+        setTitle(FRAME_TITLE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        setResizable(false);
+    }
+    
+    private void setMenu() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu index = new JMenu(INDEX_MENU_TEXT);
+        JMenu musor = new JMenu(MUSOR_MENU_TEXT);
+        JMenu film = new JMenu(FILM_MENU_TEXT);
+        JMenu ar = new JMenu(AR_MENU_TEXT);
+        JMenu kapcsolat = new JMenu(KAPCSOLAT_MENU_TEXT);
+        JMenu admin = new JMenu(ADMIN_MENU_TEXT);
+        JMenu tortenet = new JMenu(TORTENET_MENU_TEXT);
+        JMenu logout = new JMenu(LOGOUT_MENU_TEXT);
+        
+        menuBar.add(index);
+        menuBar.add(musor);
+        menuBar.add(film);
+        menuBar.add(ar);
+        menuBar.add(kapcsolat);
+        menuBar.add(admin);
+        menuBar.add(tortenet);
+        menuBar.add(logout);
+        setJMenuBar(menuBar);
+    }
+    
+        private void setCenter() {
+        JPanel panel = new JPanel(new GridLayout(1, 1));
+
+        panel.add(jPanel);
+        add(panel, BorderLayout.CENTER);
+    }
 }
---
+++ src/zoli/szakdoga/cinema/gui/GuiConstants.java
@@ -0,0 +1,22 @@
+package zoli.szakdoga.cinema.gui;
+
+/**
+ *
+ * @author pappz
+ */
+public class GuiConstants {
+    
+    public static final int FRAME_WIDTH = 650;
+    public static final int FRAME_HEIGHT = 450;
+    
+    public static final String FRAME_TITLE = "Cinema Projekt";
+    public static final String INDEX_MENU_TEXT = "Főoldal";
+    public static final String MUSOR_MENU_TEXT = "Műsor";
+    public static final String FILM_MENU_TEXT = "Filmek";
+    public static final String AR_MENU_TEXT = "Árak";
+    public static final String KAPCSOLAT_MENU_TEXT = "Kapcsolat";
+    public static final String ADMIN_MENU_TEXT = "Admin";
+    public static final String TORTENET_MENU_TEXT = "Történet";
+    public static final String LOGOUT_MENU_TEXT = "Logout";
+    
+}
